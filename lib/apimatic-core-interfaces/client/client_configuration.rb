ce48e7b200b6017bb61f463fddf2f693db461e1d
Muhammad Sufyan <muhammad.sufyan@apimatic.io>
2022-11-24 12:52:34 +0500

    Refactored the doc-block in DateTimeFormat class and removed duplicate properties in ClientConfiguration class.

--- lib/apimatic-core-interfaces/client/client_configuration.rb
+++ lib/apimatic-core-interfaces/client/client_configuration.rb
@@ -4,7 +4,7 @@ module CoreLibrary
   # This class should not be instantiated but should be used as a base class
   # for HTTP Client Configuration class.
   class ClientConfiguration
-    attr_reader :timeout, :max_retries, :retry_interval, :backoff_factor, :backoff_factor, :retry_statuses,
+    attr_reader :timeout, :max_retries, :retry_interval, :backoff_factor, :retry_statuses,
                 :retry_methods, :connection, :adapter, :response_factory, :cache, :verify
   end
 end
--- lib/apimatic-core-interfaces/types/datetime_format.rb
+++ lib/apimatic-core-interfaces/types/datetime_format.rb
@@ -1,14 +1,12 @@
 module CoreLibrary
   # Date Time Formats Enumeration.
   class DateTimeFormat
-    '' "Enumeration of Date time formats
 
-    Attributes:
-        HTTP_DATE_TIME: RFC 1123 Date-Time format
-        UNIX_DATE_TIME: Unix Timestamp Format
-        RFC3339_DATE_TIME: RFC 3339 Date-Time format
+    # Members:
+    #     HTTP_DATE_TIME: RFC 1123 Date-Time format
+    #     UNIX_DATE_TIME: Unix Timestamp Format
+    #     RFC3339_DATE_TIME: RFC 3339 Date-Time format
 
-    " ''
     DATE_TIME_FORMAT = [
       HTTP_DATE_TIME = 'HttpDateTime'.freeze,
 
